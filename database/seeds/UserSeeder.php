3e46381c229057470c3fa5346d173e6350c43526
Novianto newbie <newbie007fx@gmail.com>
2020-10-17 11:45:25 +0700

    setup base laravel project

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'customer',
+            'email' => 'customer@mail.com',
+            'password' => Hash::make('customer2020'),
+        ]);
+    }
+}
